193107025bc3e53f5f3214153e6ba5f066268ea8
Henry <henrychinjianhong@gmail.com>
2025-05-03 14:28:00 +0800

    pushTesting

--- WADAssignmentV4/screens/RewardScreen.js
+++ WADAssignmentV4/screens/RewardScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-const App = () => { return( <View style={{flex:1, justifyContent:'center', alignItems:'center'}}><Text>Reward Screen</Text></View>)}
+const App = () => { return( <View style={{flex:1, justifyContent:'center', alignItems:'center'}}><Text>Reward s</Text></View>)}
 
 export default App;
